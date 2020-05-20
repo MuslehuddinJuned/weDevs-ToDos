4baff426eb90e9df27b57769ea65e123527fc632
Muslehuddin Juned <MuslehuddinJuned@users.noreply.github.com>
2020-05-20 14:33:14 +0600

    Add footer

--- resources/views/todo.blade.php
+++ resources/views/todo.blade.php
@@ -7,5 +7,6 @@
                 :todo="{{ json_encode($todo) }}"
                 :todocomplete="{{ json_encode($todoComplete) }}">
         </todo-list>
+        <footer class="text-right col-md-10">Developed by Muslehuddin Juned. <a href="https://www.sustipe.com">www.sustipe.com</a></footer>
 </div>
 @endsection
